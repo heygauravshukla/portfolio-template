c4916c1a501d50cba51db95213aabcaf2ba973fc
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-23 17:52:53 +0530

    feat: integrate MDX support in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -7,4 +8,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);

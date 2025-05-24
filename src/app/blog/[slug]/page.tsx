b71579484a643e7898908b2b8f222375f2fc6b93
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-24 11:51:47 +0530

    feat: add Image component to BlogPage for displaying blog images

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/container";
 import { getBlogFrontMatterBySlug, getSingleBlog } from "@/utils/mdx";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export async function generateMetadata({
@@ -40,6 +41,13 @@ export default async function BlogPage({
   return (
     <div className="flex min-h-screen items-start justify-start">
       <Container className="min-h-[200vh] px-10 md:pt-20 md:pb-10">
+        <Image
+          src={frontmatter.image}
+          alt={frontmatter.title}
+          width={1000}
+          height={500}
+          className="mx-auto mb-20 max-h-96 w-full max-w-2xl rounded-2xl object-cover shadow-xl"
+        />
         <div className="prose mx-auto">{content}</div>
       </Container>
     </div>

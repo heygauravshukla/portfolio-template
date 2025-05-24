78cf8e136d2f81ffd8e245a7fd17abaa65de7ccf
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-24 10:04:51 +0530

    feat: add About page with introductory content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+import { Container } from "@/components/container";
+
+export default function About() {
+  return (
+    <div className="flex min-h-screen items-start justify-start">
+      <Container className="min-h-[200vh] px-10 md:pt-20 md:pb-10">
+        <h1 className="text-primary text-2xl font-bold tracking-tight md:text-4xl">
+          About Me
+        </h1>
+        <p className="text-secondary max-w-lg pt-4 text-sm">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet harum
+          distinctio, exercitationem repellat sed provident alias aspernatur
+          eos. Autem, quaerat! Maiores accusantium dignissimos debitis quis
+          quibusdam.
+        </p>
+      </Container>
+    </div>
+  );
+}

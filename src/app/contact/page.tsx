98063e8b262ad7820602c8390fe5777bad93efea
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-24 15:25:20 +0530

    feat: add ContactPage and ContactForm components for user inquiries

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,19 @@
+import { ContactForm } from "@/components/contact-form";
+import { Container } from "@/components/container";
+import { Heading } from "@/components/heading";
+import { Subheading } from "@/components/subheading";
+
+export default function ContactPage() {
+  return (
+    <div className="flex min-h-screen items-start justify-start">
+      <Container className="min-h-screen px-10 md:pt-20 md:pb-10">
+        <Heading>Contact Me</Heading>
+        <Subheading>
+          I'm open to freelancing offers. Reach out to me to inquire more about
+          my work.
+        </Subheading>
+        <ContactForm />
+      </Container>
+    </div>
+  );
+}
---
+++ src/components/contact-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export const ContactForm = () => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("handleSubmit clicked");
+
+    const { name, email, message } = formData;
+
+    if (!name || !email || !message) {
+      toast.error("Please fill all the fields");
+      return;
+    }
+
+    // call your API here to submit the form
+    // here's a mock API call
+
+    const response = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("API call successful!");
+      }, 1000);
+    });
+
+    if (response) {
+      toast.success("Form submitted successfully!");
+    } else {
+      toast.error("Something went wrong");
+    }
+  };
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mx-auto flex max-w-lg flex-col gap-5 py-10"
+    >
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="name"
+          className="text-sm font-medium tracking-tight text-neutral-600"
+        >
+          Full name
+        </label>
+        <input
+          id="name"
+          name="name"
+          onChange={handleChange}
+          type="text"
+          placeholder="Gaurav Shukla"
+          className="shadow-aceternity focus:ring-primary rounded-md p-2 py-1 text-sm focus:ring-2 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="email"
+          className="text-sm font-medium tracking-tight text-neutral-600"
+        >
+          Email Address
+        </label>
+        <input
+          id="email"
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="user@example.com"
+          className="shadow-aceternity focus:ring-primary rounded-md p-2 py-1 text-sm focus:ring-2 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label
+          htmlFor="message"
+          className="text-sm font-medium tracking-tight text-neutral-600"
+        >
+          Message
+        </label>
+        <textarea
+          rows={5}
+          id="message"
+          name="message"
+          onChange={handleChange}
+          placeholder="Hi Gaurav, I would like to..."
+          className="shadow-aceternity focus:ring-primary resize-none rounded-md p-2 py-1 text-sm focus:ring-2 focus:outline-none"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-primary shadow-aceternity hover:bg-primary/90 focus:ring-primary w-fit rounded-md px-4 py-2 text-sm font-medium text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:outline-none"
+      >
+        Send message
+      </button>
+    </form>
+  );
+};
